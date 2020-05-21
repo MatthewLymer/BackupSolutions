6d97439f0a888c2fc9c6da0e13d224c810698e6b
Matthew Lymer <matthew.lymer@tripstack.com>
2020-05-21 14:13:10 -0400

    Fix up the permissions

--- infrastructure/backups_bucket.tf
+++ infrastructure/backups_bucket.tf
@@ -1,16 +1,29 @@
-resource "google_storage_bucket" "backups_bucket" {
-  name = "${data.google_project.project.number}-backups"
+resource "google_storage_bucket" "matthew_pc" {
+  name = "${google_project.default.number}-backups-matthew-pc"
   location = local.region
   storage_class = "STANDARD"
 }
 
-resource "google_service_account" "backup_manager_sa" {
-  account_id = "backup-manager"
-  display_name = "Backup Manager"
+resource "google_service_account" "matthew_pc_backup_manager" {
+  account_id = "matthew-pc-backup-manager"
+  display_name = "Matthew-PC Backup Manager"
 }
 
-resource "google_storage_bucket_iam_member" "backup_manager_sa_access" {
-  bucket = google_storage_bucket.backups_bucket.name
+resource "google_project_iam_custom_role" "storage_bucket_lister" {
+  role_id = "storage_bucket_lister"
+  title = "Storage Bucket Lister"
+  permissions = [
+    "storage.buckets.list"
+  ]
+}
+
+resource "google_project_iam_member" "storage_bucket_lister" {
+  role = google_project_iam_custom_role.storage_bucket_lister.id
+  member = "serviceAccount:${google_service_account.matthew_pc_backup_manager.email}"
+}
+
+resource "google_storage_bucket_iam_member" "matthew_pc_backup_manager_access" {
+  bucket = google_storage_bucket.matthew_pc.name
   role = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.backup_manager_sa.email}"
+  member = "serviceAccount:${google_service_account.matthew_pc_backup_manager.email}"
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,8 +3,8 @@ locals {
   billing_account = "017629-BB9381-1C2281"
   
   project = {
-	id = "matthew-pc-backup"
-	name = "Matthew's PC Backup"
+	  id = "matthewlymer-computer-backups"
+	  name = "Computer Backups"
   }
 }
 
@@ -19,23 +19,9 @@ resource "google_project" "default" {
   billing_account = local.billing_account
 }
 
-data "google_project" "project" {
-}
-
-resource "google_storage_bucket" "tfstate_bucket" {
-  name = "${data.google_project.project.number}-tfstate"
-  location = local.region
-  storage_class = "STANDARD"
-}
-
 terraform {
-  # NOTE:
-  #   When first running this project, the tfstate_bucket won't exist
-  #   so comment out the backend lines, do a terraform init & apply
-  #   then uncomment out the backend lines, do another terraform init & apply
-  #   to start using the backend
-
   backend "gcs" {
-    bucket = "127296570571-tfstate"
+    bucket = "490635812867-tfstate"
+    prefix  = "matthewlymer-computer-backups"
   }
 }
